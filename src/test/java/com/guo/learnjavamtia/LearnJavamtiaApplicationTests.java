5aed6d7eb4bb6b572d94058d695da658fecd2992
guozhiwei <guo.shiwei@ebiz-fooge.com>
2019-10-28 13:53:54 +0800

    1

---
+++ src/test/java/com/guo/learnjavamtia/LearnJavamtiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guo.learnjavamtia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavamtiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
